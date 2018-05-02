13a4c36643f3beab3b6354bfec2c1e0c2b9a39d4
zc.ding <zhichaoding@hongkun.com.cn>
2018-05-02 20:40:30 +0800

    初始化dobe-parent

---
+++ src/main/java/com/dobe/dobeparent/DobeParentApplication.java
@@ -0,0 +1,12 @@
+package com.dobe.dobeparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobeParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DobeParentApplication.class, args);
+	}
+}
